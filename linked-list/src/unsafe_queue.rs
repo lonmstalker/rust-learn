d860bcf112ff510c0762221fc492fc9cac8c9da3
Никита Кочнев <nikitakocnev@MacBook-Air-Nikita.local>
2023-06-28 12:34:30 +0300

    added unsafe iter

--- linked-list/src/unsafe_queue.rs
+++ linked-list/src/unsafe_queue.rs
@@ -3,10 +3,20 @@ use std::ptr::null_mut;
 /**
 run tests:
 MIRIFLAGS="-Zmiri-tag-raw-pointers" cargo  +nightly-2023-06-18 miri test
-*/
+ */
 pub struct UnsafeQueue<T> {
     head: Link<T>,
-    tail: *mut Node<T>,
+    tail: Link<T>,
+}
+
+pub struct IntoIter<T>(UnsafeQueue<T>);
+
+pub struct Iter<'a, T> {
+    next: Option<&'a Node<T>>,
+}
+
+pub struct IterMut<'a, T> {
+    next: Option<&'a mut Node<T>>,
 }
 
 struct Node<T> {
@@ -22,7 +32,7 @@ impl<T> UnsafeQueue<T> {
     }
 
     pub fn push(&mut self, value: T) {
-        let mut new_tail = Box::into_raw(Box::new(Node {
+        let new_tail = Box::into_raw(Box::new(Node {
             value,
             next: null_mut(),
         }));
@@ -52,6 +62,70 @@ impl<T> UnsafeQueue<T> {
 
                 Some(head.value)
             }
+        }
+    }
+
+    pub fn peek(&self) -> Option<&T> {
+        unsafe {
+            self.head.as_ref().map(|node| &node.value)
+        }
+    }
+
+    pub fn peek_mut(&mut self) -> Option<&mut T> {
+        unsafe {
+            self.head.as_mut().map(|node| &mut node.value)
+        }
+    }
+}
+
+impl<T> UnsafeQueue<T> {
+    pub fn iter(&self) -> Iter<'_, T> {
+        unsafe {
+            Iter { next: self.head.as_ref() }
+        }
+    }
+
+    pub fn iter_mut(&mut self) -> IterMut<'_, T> {
+        unsafe {
+            IterMut { next: self.head.as_mut() }
+        }
+    }
+
+    pub fn into_iter(self) -> IntoIter<T> {
+        IntoIter(self)
+    }
+}
+
+impl<T> Iterator for IntoIter<T>  {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.pop()
+    }
+}
+
+impl<'a, T> Iterator for Iter<'a, T>  {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        unsafe {
+            self.next.map(|node| {
+                self.next = node.next.as_ref();
+                &node.value
+            })
+        }
+    }
+}
+
+impl<'a, T> Iterator for IterMut<'a, T>  {
+    type Item = &'a mut T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        unsafe {
+            self.next.take().map(|node| {
+                self.next = node.next.as_mut();
+                &mut node.value
+            })
         }
     }
 }
@@ -102,5 +176,43 @@ mod test {
         assert_eq!(list.pop(), Some(6));
         assert_eq!(list.pop(), Some(7));
         assert_eq!(list.pop(), None);
+    }
+
+    #[test]
+    fn miri_food() {
+        let mut list = UnsafeQueue::new();
+
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        assert_eq!(list.pop(), Some(1));
+        list.push(4);
+        assert_eq!(list.pop(), Some(2));
+        list.push(5);
+
+        assert_eq!(list.peek(), Some(&3));
+        list.push(6);
+        list.peek_mut().map(|x| *x *= 10);
+        assert_eq!(list.peek(), Some(&30));
+        assert_eq!(list.pop(), Some(30));
+
+        for elem in list.iter_mut() {
+            *elem *= 100;
+        }
+
+        let mut iter = list.iter();
+        assert_eq!(iter.next(), Some(&400));
+        assert_eq!(iter.next(), Some(&500));
+        assert_eq!(iter.next(), Some(&600));
+        assert_eq!(iter.next(), None);
+        assert_eq!(iter.next(), None);
+
+        assert_eq!(list.pop(), Some(400));
+        list.peek_mut().map(|x| *x *= 10);
+        assert_eq!(list.peek(), Some(&5000));
+        list.push(7);
+
+        // Drop it on the ground and let the dtor exercise itself
     }
 }
